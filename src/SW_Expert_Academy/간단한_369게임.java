bfddb3588f1ebfc635d770b823a16127072a5d58
JungYeon <jyp75580@gmail.com>
2023-11-01 21:15:50 +0900

    SW Expert Academy - 간단한 369게임(D2)

---
+++ src/SW_Expert_Academy/간단한_369게임.java
@@ -0,0 +1,38 @@
+package SW_Expert_Academy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 간단한_369게임 {
+
+    static BufferedReader br;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            int count = validation(i);
+            if (count == 0) sb.append(i);
+            while (count-- > 0) {
+                sb.append("-");
+            }
+            sb.append(" ");
+        }
+        System.out.println(sb);
+    }
+
+    private static int validation(int num) {
+        int count = 0;
+        char[] arr = String.valueOf(num).toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == '3' || arr[i] == '6' || arr[i] == '9') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
