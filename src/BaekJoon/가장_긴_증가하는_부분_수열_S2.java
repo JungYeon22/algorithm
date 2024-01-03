c11e9d8be1b2eb972c00238d4c383ab6be5fd0b3
JungYeon22 <jyp7558@gmail.com>
2024-01-03 20:01:23 +0900

    BaekJoon - 가장 긴 증가하는 부분 수열 S2

---
+++ src/BaekJoon/가장_긴_증가하는_부분_수열_S2.java
@@ -0,0 +1,36 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 가장_긴_증가하는_부분_수열_S2 {
+    static int[] arr;
+    static int[] dp;
+    static int max, N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        dp = new int[N];
+        String[] split = br.readLine().split(" ");
+        for(int i=0;i < N;i++){
+            arr[i] = Integer.parseInt(split[i]);
+        }
+
+        for(int i=0;i < N;i++){
+            dp[i] = 1;
+            for(int j=0;j < i;j++){
+                if(arr[i] > arr[j] && dp[i] < dp[j] + 1){
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+        for(int data : dp){
+            if(max < data) max = data;
+        }
+        System.out.println(max);
+
+    }
+
+}
