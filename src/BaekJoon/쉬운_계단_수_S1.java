840073f57f607eadd97cb5e648f9a1b7239945f7
JungYeon22 <jyp7558@gmail.com>
2024-01-05 17:31:01 +0900

    BaekJoon - 쉬운 계단 수 S1

--- src/BaekJoon/쉬운_계단_수_S1.java
+++ src/BaekJoon/쉬운_계단_수_S1.java
@@ -1,40 +1,44 @@
 package BaekJoon;
 
-import javax.swing.plaf.basic.BasicButtonUI;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class 쉬운_계단_수_S1 {
     static BufferedReader br;
-    static StringTokenizer st;
-    static int count;
     static int N;
-    static int[] stairsNum;
+    static Long[][] dp;
 
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        stairsNum = new int[N];
-        count = 0;
-        // 1 ~ 9
-        for (int i = 1; i < 10; i++) {
-            solution(i, 0);
+        dp = new Long[N+1][10];
+        for(int i=0;i < 10;i++){
+            dp[1][i] = 1L;
         }
-        System.out.println(count % 1000000000);
 
+        long result =0L;
+        for(int i=1;i < 10;i++){
+            result += DP(N,i);
+        }
+        System.out.println(result % 1000000000);
     }
 
-    private static void solution(int num, int index) {
-        if (num < 0 || num > 9) return;
-        if (index == N-1) {
-            count++;
-            return;
+    private static long DP(int now, int num) {
+        if(now == 1){
+            return dp[now][num];
         }
-        solution(num + 1, index + 1);
-        solution(num - 1, index + 1);
-
-
+        if(dp[now][num] == null){
+            if(num == 0) {
+                dp[now][num] = DP(now-1, 1);
+            }
+            else if(num == 9){
+                dp[now][num] = DP(now-1, 8);
+            }else {
+                dp[now][num] = DP(now-1, num-1) + DP(now-1, num+1);
+            }
+        }
+        return dp[now][num] % 1000000000;
     }
+
 }
