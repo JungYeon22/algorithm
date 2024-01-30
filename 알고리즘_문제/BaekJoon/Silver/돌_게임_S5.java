a0fec346238d660f65fda7e519ff2ab56b8ed7bf
JungYeon22 <jyp7558@gmail.com>
2024-01-30 20:09:24 +0900

    BaekJoon - 돌 게임 S5

---
+++ 알고리즘_문제/BaekJoon/Silver/돌_게임_S5.java
@@ -0,0 +1,18 @@
+package BaekJoon.Silver;
+
+import java.util.Scanner;
+
+public class 돌_게임_S5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+
+        if(N%2 == 0) System.out.println("CY");
+        else System.out.println("SK");
+
+
+
+    }
+
+}
